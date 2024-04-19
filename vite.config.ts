9161ecdb5f68cf87b3db16656af392662f0eb24a
ReeseHatfield <hatfield.69@wright.edu>
2024-04-19 12:14:08 -0400

    initial gh-pages setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
+const repoName = "SeniorDesignWebsite";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.NODE_ENV === 'production' ? `/${repoName}/` : '/'
 })
