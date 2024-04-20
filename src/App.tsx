3f4cb391ee64eba5e9e508150d173014ec614dba
ReeseHatfield <hatfield.69@wright.edu>
2024-04-20 18:36:29 -0400

    setup basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import Login from './pages/Login';
 
 function App() {
 
   return (
-    <>
-      <h1>2024 Senior Design Website</h1>
-      <span> Images/username/password/video stuff will go here</span>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/login' element={<Login />}> </Route>
+        <Route path='/' element={<Login />}></Route>
+      </Routes>
+    </BrowserRouter>
   )
+
+
+  // return (
+  //   <>
+  //     <h1>2024 Senior Design Website</h1>
+  //     <span> Images/username/password/video stuff will go here</span>
+  //   </>
+  // )
 }
 
 export default App
